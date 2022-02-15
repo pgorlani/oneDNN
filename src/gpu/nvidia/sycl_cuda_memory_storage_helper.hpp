e8e264b9fb5b4181f6388214d6f6d9a640eaf516
Tadej Ciglarič <tadej.ciglaric@codeplay.com>
2022-02-15 09:11:33 +0000

    gpu: cuda: updated memory helper class

--- src/gpu/nvidia/sycl_cuda_memory_storage_helper.hpp
+++ src/gpu/nvidia/sycl_cuda_memory_storage_helper.hpp
@@ -28,22 +28,22 @@ namespace gpu {
 namespace nvidia {
 
 #define CTX_IN_SYCL_MEMORY(arg) \
-    sycl_memory_arg<::sycl::access::mode::read>( \
+    sycl_memory_arg_t<::sycl::access::mode::read>( \
             static_cast<sycl::sycl_memory_storage_base_t *>( \
                     &CTX_IN_STORAGE(arg)), \
             cgh)
 
 #define CTX_OUT_SYCL_MEMORY(arg) \
-    sycl_memory_arg<::sycl::access::mode::write>( \
+    sycl_memory_arg_t<::sycl::access::mode::write>( \
             static_cast<sycl::sycl_memory_storage_base_t *>( \
                     &CTX_OUT_STORAGE(arg)), \
             cgh)
 
 template <::sycl::access_mode mode>
-class sycl_memory_arg {
+class sycl_memory_arg_t {
 public:
-    sycl_memory_arg() = default;
-    sycl_memory_arg(
+    sycl_memory_arg_t() = default;
+    sycl_memory_arg_t(
             sycl::sycl_memory_storage_base_t *mem, ::sycl::handler &cgh) {
         switch (mem->memory_kind()) {
             case sycl::memory_kind::buffer:
@@ -54,35 +54,37 @@ public:
                         cgh);
                 break;
             case sycl::memory_kind::usm:
-                usm_mem_ = utils::downcast<
-                        const sycl::sycl_usm_memory_storage_t *>(mem);
+                raw_ptr_ = utils::downcast<
+                        const sycl::sycl_usm_memory_storage_t *>(mem)
+                                   ->usm_ptr();
                 break;
             default: assert(!"unexpected memory kind");
         }
     }
 
-    template <typename T = void>
-    T *get_native_pointer(const compat::interop_handle &ih,
-            const cuda_sycl_scoped_context_handler_t &sc) const {
+    template <::sycl::backend be = ::sycl::backend::ext_oneapi_cuda,
+            typename T = void>
+    T *get_native_pointer(const compat::interop_handle &ih) const {
         void *raw_ptr;
-        if (usm_mem_) {
-            raw_ptr = usm_mem_->usm_ptr();
+        if (acc_.has_value()) {
+            raw_ptr = reinterpret_cast<void *>(ih.get_native_mem<be>(acc_.value()));
         } else {
-            raw_ptr = sc.memory<void *>(ih, acc_.value());
+            raw_ptr = raw_ptr_;
         }
         return reinterpret_cast<T *>(raw_ptr);
     }
 
-    template <typename T = void>
-    T *get_native_pointer(const compat::interop_handle &ih,
-            const cuda_sycl_scoped_context_handler_t &sc, size_t offset) const {
+    template <::sycl::backend be = ::sycl::backend::ext_oneapi_cuda,
+            typename T = void>
+    T *get_native_pointer(
+            const compat::interop_handle &ih, size_t offset) const {
         return reinterpret_cast<T *>(
-                reinterpret_cast<uint8_t *>(get_native_pointer<T>(ih, sc))
+                reinterpret_cast<uint8_t *>(get_native_pointer<be, T>(ih))
                 + offset);
     }
 
 private:
-    const sycl::sycl_usm_memory_storage_t *usm_mem_ = nullptr;
+    void *raw_ptr_ = nullptr;
     std::optional<::sycl::accessor<uint8_t, 1, mode>> acc_;
 };
 
